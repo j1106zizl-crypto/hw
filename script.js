c908fd404787043737582867de346e09a232fb66
janesv <j1106zizl@gmail.com>
2026-05-06 11:25:29 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function square() {
+  document.getElementById("box").style.background = "red";
+  document.getElementById("box").style.borderRadius = "0";
+}
+function circle() {
+  document.getElementById("box").style.background = "red";
+  document.getElementById("box").style.borderRadius = "50%";
+}
+function deletebox() {
+  document.getElementById("box").style.display = "none";
+}
